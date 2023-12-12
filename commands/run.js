3c22691fc735b8bbd7df13d844bf28ad5dbf86b8
Cédric BOLLARO <cedric.bollaro@hotmail.fr>
2023-12-12 09:59:22 +0100

    fix: (cors) prevent app crashing on multiple calls

--- commands/run.js
+++ commands/run.js
@@ -5,27 +5,27 @@ const corsMiddleware = require("cors");
 function run ({ uri, port, cors }) {
     const proxy = httpProxy.createProxyServer({});
 
-    const server = http.createServer(function(req, res) {
-        if (cors) {
-            let corsOptions = {}
+    if (cors) {
+        let corsOptions = {}
 
-            if (Array.isArray(cors)) {
-                const whitelist = Array.isArray(cors)
+        if (Array.isArray(cors)) {
+            const whitelist = Array.isArray(cors)
 
-                corsOptions.origin = function (origin, callback) {
-                    if (whitelist.indexOf(origin) !== -1) {
-                        callback(null, true)
-                    } else {
-                        callback(new Error('Not allowed by CORS'))
-                    }
+            corsOptions.origin = function (origin, callback) {
+                if (whitelist.indexOf(origin) !== -1) {
+                    callback(null, true)
+                } else {
+                    callback(new Error('Not allowed by CORS'))
                 }
             }
-
-            proxy.on('proxyRes', (proxyRes, req, res) => {
-                corsMiddleware(corsOptions)(req, res, () => {});
-            });
         }
 
+        proxy.on('proxyRes', (proxyRes, req, res) => {
+            corsMiddleware(corsOptions)(req, res, () => {});
+        });
+    }
+
+    const server = http.createServer(function(req, res) {
         proxy.web(req, res, {
             changeOrigin: true,
             target: uri
